9dd285d645168b6b0790e3582985c4e2969efb5a
joao horacio do lago <joaolago35@gmail.com>
2023-01-12 08:18:05 -0300

    estudos sobre designer patterns rinaldodev aprendizado

---
+++ src/br/com/rinaldo/designer/pattern/Adapter_2.java
@@ -0,0 +1,64 @@
+package br.com.rinaldo.designer.pattern;
+
+import java.math.BigDecimal;
+
+public class Adapter_2 {
+    // Client
+    // Adapter
+    // Adaptee
+    // Target
+
+    public void main(String[] args) {
+        ProcessorPagamento2 credito = new MeuPagamentoCredito2();
+        credito.debitar(new BigDecimal("100"));
+    }
+    //----------------------------------------------------
+
+    interface ProcessorPagamento2 {
+
+        void debitar(BigDecimal valor);
+
+        void creditar(BigDecimal valor);
+
+    }
+
+//----------------------------------------------------
+
+    class MeuPagamentoCredito2 implements ProcessorPagamento2 {
+
+        SdkPagamentoCredito2 sdkPagamentoCredito = new SdkPagamentoCredito2();
+
+        // ..
+
+        public void debitar(BigDecimal valor) {
+            // ..
+            sdkPagamentoCredito.autorizar(valor);
+            sdkPagamentoCredito.capturar(valor);
+        }
+
+        public void creditar(BigDecimal valor) {
+            // ..
+            sdkPagamentoCredito.creditar(valor);
+        }
+
+    }
+
+//----------------------------------------------------
+
+    class SdkPagamentoCredito2 { // .jar / lib no maven
+
+        // posso até ter acesso, mas não convém alterar
+
+        void autorizar(BigDecimal valor) {
+            // autoriza
+        }
+
+        void capturar(BigDecimal valor) {
+            // captura
+        }
+
+        void creditar(BigDecimal valor) {
+            // credita
+        }
+    }
+}
---
+++ src/br/com/rinaldo/designer/pattern/Adapter_3.java
@@ -0,0 +1,59 @@
+package br.com.rinaldo.designer.pattern;
+
+import java.math.BigDecimal;
+
+public class Adapter_3 {
+    public void main(String[] args) {
+        MeuPagamentoCredito3 credito = new MeuPagamentoCredito3();
+        credito.debitar(new BigDecimal("100"));
+    }
+    //----------------------------------------------------
+
+    interface ProcessorPagamento3 {
+
+        void debitar(BigDecimal valor);
+
+        void creditar(BigDecimal valor);
+
+    }
+
+//----------------------------------------------------
+
+    class MeuPagamentoCredito3 extends SdkPagamentoCredito3
+            implements ProcessorPagamento3 {
+
+        // ...
+
+        public void debitar(BigDecimal valor) {
+            super.autorizar(valor);
+            super.capturar(valor);
+        }
+        @Override
+        public void creditar(BigDecimal valor) {
+            super.creditar(valor);
+        }
+
+//    @Override
+//    void autorizar(BigDecimal valor) {
+//        // mudou o comportamento - não é mais two-way
+//    }
+
+    }
+
+//----------------------------------------------------
+
+    class SdkPagamentoCredito3 {
+
+        void autorizar(BigDecimal valor) {
+            // autoriza
+        }
+
+        void capturar(BigDecimal valor) {
+            // captura
+        }
+
+        void creditar(BigDecimal valor) {
+            // credita
+        }
+    }
+}
